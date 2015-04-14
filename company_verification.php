1a2a711844495e846922403a2f50f4a4a99ba89f
vishwerine <vishwash.iitrpr@gmail.com>
2015-04-14 09:41:45 +0530

    first commit

---
+++ company_verification.php
@@ -0,0 +1,26 @@
+<?php
+
+include "db.php";
+session_start();
+$conn = get_conn();
+
+$sql ="select * from company where cid='".$_REQUEST['cid']."' ;" ;
+
+$q = $conn->query($sql) or die('failed');
+
+$r = $q->fetch(PDO::FETCH_ASSOC);
+
+if ($r['password'] == $_REQUEST['password'])
+ {
+  
+  #echo "correct password";
+  $_SESSION['cid'] = $_REQUEST['cid'];
+  header("Location: welcome_company.php");
+ }
+else
+ {
+  echo "incorrect password";
+ }
+
+
+?>
